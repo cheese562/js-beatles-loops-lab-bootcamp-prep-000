28e0a795ce9db6800692525f884fc8fabef17328
Leon K <leonknight@outlook.com>
2018-02-23 05:51:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function theBeatlesPlay(musicians, instruments)
+{
+  // create empty array stored in a variable
+  var array = []
+  
+  // loop over array of musicians
+  for (let i = 0; i < musicians.length; i++)
+  {
+    array.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  
+  return array
+}
